63cabcb0b7e78bb51cff48f57612bd0f512814b5
OksanaPugach <oksana.pugach.it@gmail.com>
2020-10-24 23:53:28 +0200

    init:update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+  module.exports = function check(str, bracketsConfig) {
+  const openBracket = bracketsConfig.map((item) => item[0]);
+  const closeBraket = bracketsConfig.map((item) => item[1]);
+  const caretaker = [];
+  for (let i = 0; i < str.length; i += 1) {
+    const openIndex = openBracket.indexOf(str[i]);
+    const closeIndex = closeBraket.indexOf(str[i]);
+
+    if (closeIndex === openIndex) {
+      if (caretaker.length > 0 && caretaker[caretaker.length - 1] === closeIndex) {
+        caretaker.pop()
+      } else {
+        caretaker.push(openIndex);
+      }
+    } else if (openIndex !== -1) {
+      caretaker.push(openIndex)
+    } else {
+      if (caretaker[caretaker.length - 1] === closeIndex) {
+        caretaker.pop();
+      } else return false;
+    }
+  }
+  
+  return caretaker.length === 0;
 }
